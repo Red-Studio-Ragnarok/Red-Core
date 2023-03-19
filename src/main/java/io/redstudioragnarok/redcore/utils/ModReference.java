a1fa4999d514b166c69d5fdad299375fc1bc3fa9
Desoroxxx <desoroxxx@gmail.com>
2023-03-19 17:56:25 +0100

    Fix Gradle Breaking The Name

--- src/main/java/io/redstudioragnarok/redcore/utils/ModReference.java
+++ src/main/java/io/redstudioragnarok/redcore/utils/ModReference.java
@@ -5,11 +5,13 @@ import org.apache.logging.log4j.Logger;
 
 /**
  * This class defines constants for Red Core.
+ * <p>
+ * They are automatically updated by Gradle on compile time, except for the name as Gradle would remove spaces.
  */
 public class ModReference {
 
     public static final String id = "@ID@";
-    public static final String name = "@NAME@";
+    public static final String name = "Red Core";
     public static final String version = "@VERSION@";
     public static final Logger log = LogManager.getLogger(id);
 }
