89cf75198ecbcfa00ef0de198c4cdcade6db477b
Desoroxxx <desoroxxx@gmail.com>
2023-03-19 16:06:44 +0100

    Refactor `Chronometer` To `Stopwatch`

--- src/main/java/io/redstudioragnarok/redcore/utils/Stopwatch.java
+++ src/main/java/io/redstudioragnarok/redcore/utils/Stopwatch.java
@@ -6,22 +6,22 @@ import java.util.Map;
 /**
  * A utility class for measuring elapsed time in milliseconds between two points in the code.
  */
-public class Chronometer {
+public class Stopwatch {
 
     /**
-     * A private static variable that keeps track of the next ID to assign to a newly started chronometer.
+     * A private static variable that keeps track of the next ID to assign to a newly started stopwatch.
      */
     private static int nextId = 0;
 
     /**
-     * A Map that stores the start time of each chronometer identified by its ID.
+     * A Map that stores the start time of each stopwatch identified by its ID.
      */
     private static final Map<Integer, Long> startTimes = new HashMap<>();
 
     /**
-     * Starts a new chronometer and returns its unique ID.
+     * Starts a new stopwatch and returns its unique ID.
      *
-     * @return The ID of the newly started chronometer
+     * @return The ID of the newly started stopwatch
      */
     public static int start() {
         int id = nextId++;
@@ -33,9 +33,9 @@ public class Chronometer {
     }
 
     /**
-     * Stops the chronometer with the given ID and logs the elapsed time in milliseconds.
+     * Stops the stopwatch with the given ID and logs the elapsed time in milliseconds.
      *
-     * @param id The ID of the chronometer to stop
+     * @param id The ID of the stopwatch to stop
      */
     public static void stop(int id) {
         ModReference.log.info("Time elapsed for chronometer with id " + id + ": " + (System.nanoTime() - startTimes.get(id)) / 1_000_000 + "ms");
