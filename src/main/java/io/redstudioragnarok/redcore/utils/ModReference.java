261c32da90f30159b39ad30e66781fd363797e30
Desoroxxx <desoroxxx@gmail.com>
2023-06-04 15:20:16 +0200

    Switch to RFG

--- src/main/java/io/redstudioragnarok/redcore/utils/ModReference.java
+++ src/main/java/io/redstudioragnarok/redcore/utils/ModReference.java
@@ -1,5 +1,6 @@
 package io.redstudioragnarok.redcore.utils;
 
+import io.redstudioragnarok.redcore.Tags;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,8 +11,8 @@ import org.apache.logging.log4j.Logger;
  */
 public class ModReference {
 
-    public static final String id = "@ID@";
+    public static final String id = Tags.ID;
     public static final String name = "Red Core";
-    public static final String version = "@VERSION@";
+    public static final String version = Tags.VERSION;
     public static final Logger log = LogManager.getLogger(id);
 }
