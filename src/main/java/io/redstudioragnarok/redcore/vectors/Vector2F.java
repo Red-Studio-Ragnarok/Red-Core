647b36f761b0d0dd2c16cdacdaabbfaebb85e264
Desoroxxx <desoroxxx@gmail.com>
2023-06-06 02:14:22 +0200

    Vectors!

---
+++ src/main/java/io/redstudioragnarok/redcore/vectors/Vector2F.java
@@ -0,0 +1,31 @@
+package io.redstudioragnarok.redcore.vectors;
+
+/**
+ * Represents a two-dimensional vector with `x` and `y` coordinates using float.
+ *
+ * @author Desoroxxx
+ */
+public class Vector2F {
+
+    /** The x component of this vector. */
+    public float x;
+    /** The y component of this vector. */
+    public float y;
+
+    /**
+     * Constructs a new empty vector.
+     */
+    public Vector2F() {
+    }
+
+    /**
+     * Constructs a new vector with the given x and y coordinates.
+     *
+     * @param inputX The X coordinate
+     * @param inputY The Y coordinate
+     */
+    public Vector2F(final float inputX, final float inputY) {
+        x = inputX;
+        y = inputY;
+    }
+}
