bbbbf79f7ea61272ca6f206e399d2eea90fb6196
Desoroxxx <desoroxxx@gmail.com>
2023-12-04 12:31:32 +0100

    Update RFG

--- mc/1-7---1-12/build.gradle.kts
+++ mc/1-7---1-12/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.gtnewhorizons.retrofuturagradle") version "1.3.24"
+    id("com.gtnewhorizons.retrofuturagradle") version "1.3.26"
 }
 
 val jarBaseName = "!Red-Core-MC-1.7-1.12"
